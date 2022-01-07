ea52694a527d223e5cb8ca6f00ba50bd55502e21
endymion1818 <endymion1818@gmail.com>
2022-01-07 16:42:18 +0000

    mock claim

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express();
+
+const mockClaim = {
+    isSuccess: true,
+    hasSubmittedClaim: true
+}
+
+app.get('/', (req, res) => {
+    return res.send('ok')
+});
+
+app.get('/:orderid', (req, res) => {
+    return res.send(JSON.stringify(mockClaim))
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`app listening on ${process.env.PORT}`);
+})
